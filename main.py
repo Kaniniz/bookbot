db59ea4eccb66e27d0c101aee5066f82adda6255
Kaniniz <majt1211@gmail.com>
2025-04-06 15:57:45 +0200

    main.py now prints the contents file books/frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as file:
+        book = file.read()
+        return book
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
