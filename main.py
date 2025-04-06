a2c19a08748f994a083b09ff97cc5e7dd67ee376
Kaniniz <majt1211@gmail.com>
2025-04-06 17:53:05 +0200

    main.py now generates a report of books/frankenstein.txt, stats.py updated with new functions

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from stats import count_words
-from stats import get_word_count
-
-def get_book_text(file_path):
-    with open(file_path) as file:
-        book = file.read()
-        return book
+from stats import generate_report
 
 def main():
-    print(f"{count_words(get_book_text('books/frankenstein.txt'))} words found in the document")
-    print(get_word_count(get_book_text('books/frankenstein.txt')))
+    generate_report("books/frankenstein.txt")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+def get_book_text(file_path):
+    with open(file_path) as file:
+        book = file.read()
+        return book
+
 def count_words(text):
     word_count = 0
     words = text.split()
@@ -18,3 +23,29 @@ def get_word_count(text):
             
     return word_count
 
+def sort_on(dict):
+    return dict["num"]
+
+
+def sort_dict(dict):
+    sorted = []
+    for i in dict:
+        temp = {}
+        temp["name"] = i
+        temp["num"] = dict[i]
+        sorted.append(temp)
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
+
+def generate_report(file_path):
+    text = get_book_text(file_path)
+    words = get_word_count(text)
+    sorted_words = sort_dict(words)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(text)} total words")
+    print("--------- Character Count -------")
+    for I in sorted_words:
+        print(f"{I['name']}: {I['num']}")
+    print("============= END ===============")
